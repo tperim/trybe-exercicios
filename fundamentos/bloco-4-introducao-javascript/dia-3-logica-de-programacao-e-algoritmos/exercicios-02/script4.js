fc8a7c0474fbe2a892da3f69141c1db20d82de5d
Thiago Perim <tperim@gmail.com>
2022-08-22 17:12:46 -0300

    Mais um exercicio

--- fundamentos/bloco-4-introducao-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicios-02/script4.js
+++ fundamentos/bloco-4-introducao-javascript/dia-3-logica-de-programacao-e-algoritmos/exercicios-02/script4.js
@@ -1,12 +1,12 @@
 // Escreva dois algoritmos: um que retorne a maior palavra deste array e outro que retorne a menor. Considere o número de caracteres de cada palavra.
-// Retorna a maior palavra
+// Retorna a menor palavra
 let array = ['java', 'javascript', 'python', 'html', 'css'];
 let palavra='';
 let caracterElemento = 0;
 
 for (elemento of array){
     
-    if (caracterElemento === 0 || elemento.length>caracterElemento){
+    if (caracterElemento === 0 || elemento.length<caracterElemento){
         caracterElemento=elemento.length;
         palavra=elemento;
     }
