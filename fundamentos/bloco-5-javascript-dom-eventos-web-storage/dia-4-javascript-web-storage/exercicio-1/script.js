bd911ed06e614ffb5bbd60fd3060e30fda7b5f34
Thiago Perim <tperim@gmail.com>
2022-09-13 18:57:47 -0300

    Exercicios do ultimo dia do bloco 6

--- fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-4-javascript-web-storage/exercicio-1/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-4-javascript-web-storage/exercicio-1/script.js
@@ -3,7 +3,7 @@ const input = document.getElementById('phrases-input');
 const list = document.getElementById('phrases-list');
 
 function insertPhraseInDOM() {
-  const phrasesList = JSON.parse(localStorage.getItem('phrases'));
+  const phrasesList = JSON.parse(sessionStorage.getItem('phrases'));
   const listLength = phrasesList.length - 1;
   const phraseText = phrasesList[listLength];
   const phrase = document.createElement('li');
@@ -11,30 +11,15 @@ function insertPhraseInDOM() {
   list.appendChild(phrase);
 }
 
-function addPhraseToLocalStorage() {
-  const oldList = JSON.parse(localStorage.getItem('phrases'));
+function addPhraseToSessionStorage() {
+  if (sessionStorage.getItem('phrases') === null) {
+    sessionStorage.setItem('phrases', JSON.stringify([]));
+  }
+  const oldList = JSON.parse(sessionStorage.getItem('phrases'));
   const phraseText = input.value;
   oldList.push(phraseText);
-  localStorage.setItem('phrases', JSON.stringify(oldList));
+  sessionStorage.setItem('phrases', JSON.stringify(oldList));
   insertPhraseInDOM();
 }
 
-function initialRenderization() {
-  if (localStorage.getItem('phrases') === null) {
-    localStorage.setItem('phrases', JSON.stringify([]));
-  } else {
-    const phrasesList = JSON.parse(localStorage.getItem('phrases'));
-    const listLength = phrasesList.length - 1;
-    for (let index = 0; index <= listLength; index += 1) {
-      const listElement = document.createElement('li');
-      listElement.innerText = phrasesList[index];
-      list.appendChild(listElement);
-    }
-  }
-}
-
-button.addEventListener('click', addPhraseToLocalStorage);
-
-window.onload = function() {
-  initialRenderization();
-};
+button.addEventListener('click', addPhraseToSessionStorage);
---
+++ fundamentos/bloco-5-javascript-dom-eventos-web-storage/dia-4-javascript-web-storage/exercicio-2/script.js
@@ -0,0 +1,7 @@
+function inner (selectID) {
+    const selector = document.getElementById(selectID);
+    console.log(selector.innerHTML);
+  }
+
+  console.log(inner("baixo"));
+  
