b485a0f7500ef245600c88371c48eef4b15b050f
Thiago Perim <tperim@gmail.com>
2022-09-19 15:38:05 -0300

    Iniciando exercicios

---
+++ fundamentos/bloco-7-introducao-javascript-ec6-testes-unitarios/dia-2-javascript-es6-fluxo-excecao-objetos/exercicio-1/script3.js
@@ -0,0 +1,7 @@
+const top3Albums = {
+  1: 'Nightfall in Middle-Earth',
+  2: 'Imaginations from the Other Side',
+  3: 'Somewhere Far Beyond',
+};
+
+console.log(Object.entries(top3Albums));
---
+++ fundamentos/bloco-7-introducao-javascript-ec6-testes-unitarios/dia-2-javascript-es6-fluxo-excecao-objetos/exercicio-1/script6.js
@@ -0,0 +1,14 @@
+const band = {
+    name: 'Blind Guardian',
+    formedIn: 1986,
+    lastAlbum: 'Beyond the Red Mirror',
+  };
+  
+  const info = {
+    genre: 'Power Metal',
+    lastAlbum: 'Twilight Orchestra: Legacy of the Dark Lands',
+  };
+
+  teste = Object.assign(band, info)
+
+  console.log(teste);
